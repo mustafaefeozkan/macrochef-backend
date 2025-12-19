0b7500869f6c476a73d4a5a0413e1a9c5f731976
mustafaefeozkan <mustafaozkan659@gmail.com>
2025-12-19 23:14:40 +0300

    Added debug logs

--- src/main/java/com/macrochef/MacrochefApplication.java
+++ src/main/java/com/macrochef/MacrochefApplication.java
@@ -1,12 +1,26 @@
 package com.macrochef;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication(scanBasePackages = "com.macrochef")
+@SpringBootApplication
 public class MacrochefApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MacrochefApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner logDatabaseUrl(Environment env) {
+        return args -> {
+            System.out.println("=========================================");
+            System.out.println("DEBUG - DB_URL: " + env.getProperty("DB_URL"));
+            System.out.println("DEBUG - DATASOURCE URL: " + env.getProperty("spring.datasource.url"));
+            System.out.println("=========================================");
+        };
     }
 }
