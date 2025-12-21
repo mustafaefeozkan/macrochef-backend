3ca0f78acf58fb9bb790a26bb2aaaa2a5eaecd37
mustafaefeozkan <mustafaozkan659@gmail.com>
2025-12-21 17:19:52 +0300

    Security config update: Malzeme ve urun endpointleri halka acildi

--- src/main/java/com/macrochef/config/SecurityConfig.java
+++ src/main/java/com/macrochef/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                                 "/index.html",
                                 "/api/auth/**",
                                 "/auth/**",
+                                "/api/ingredients/**",
+                                "/api/products/**",
                                 "/api/categories/**",
                                 "/css/**",
                                 "/js/**",
